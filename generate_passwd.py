9d53b8656fdba603141d37731b461f781bd4755f
Accepted Errors <103551509+AcceptedErrors@users.noreply.github.com>
2022-10-27 16:57:32 +0700

    Update generate_passwd.py

--- generate_passwd.py
+++ generate_passwd.py
@@ -2,9 +2,9 @@ import string
 import random
 
 def generate_pw(pwLen=25, chars=string.ascii_letters + string.digits + string.punctuation):
-  return ''.join([random.choice(chars) for x in range(pwLen)])
+  return "".join([random.choice(chars) for x in range(pwLen)])
 
 if __name__ == "__main__":
 	print("Generated password:")
 	for i in range(1, 4):
-		print(f" #{i} {generate_pw(65)}")
+		print(f" #{i} {generate_pw()}")
